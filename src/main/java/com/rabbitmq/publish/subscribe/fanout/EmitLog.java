8e6b0165e189ca758354573a8e9cda7b07f15ed0
zhangxingj <caption2005@163.com>
2018-11-20 15:56:29 +0800

    submit

---
+++ src/main/java/com/rabbitmq/publish/subscribe/fanout/EmitLog.java
@@ -0,0 +1,32 @@
+package com.rabbitmq.publish.subscribe.fanout;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory=new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection=factory.newConnection();
+        Channel channel=connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME,"fanout");//fanout表示分发，所有的消费者得到同样的队列信息
+        //分发信息
+        for (int i=0;i<5;i++){
+            String message="Hello World"+i;
+            channel.basicPublish(EXCHANGE_NAME,"",null,message.getBytes());
+            System.out.println("EmitLog Sent '" + message + "'");
+        }
+        channel.close();
+        connection.close();
+    }
+}
---
+++ src/main/java/com/socket/ReceiveClient.java
@@ -0,0 +1,9 @@
+package com.socket;
+
+public class ReceiveClient {
+
+	public ReceiveClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
